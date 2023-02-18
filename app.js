e04c2630a8c84f045a4b107017e1798469671c06
Andrii_Petrov <Andrii_Petrov@epam.com>
2023-02-18 17:03:33 +0100

    Simple websocket server (https://www.piesocket.com/blog/nodejs-websocket)

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
-const http = require('http');
+// Importing the required modules
+const WebSocketServer = require('ws');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Creating a new websocket server
+const wss = new WebSocketServer.Server({ port: 8080 })
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+// Creating connection using websocket
+wss.on("connection", ws => {
+    console.log("new client connected");
+
+    // sending message to client
+    ws.send('Welcome, you are connected!');
+
+    //on message from client
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`);
+        // let myVar = null;
+        if(data == 'start') {
+            myVar = setInterval(function(){ timer() }, 1000);
+        } else if (data == 'stop') {
+            clearInterval(myVar);
+        } else {
+            ws.send(`Unknown command: ${data}`);
+        }
+    });
+
+    function timer() {
+        ws.send(getTimestampInSeconds());
+    }
+
+    function getTimestampInSeconds () {
+        return Math.floor(Date.now() / 1000)
+    }
+
+    // handling what to do when clients disconnects from server
+    ws.on("close", () => {
+        console.log("the client has connected");
+    });
+
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
 });
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log("The WebSocket server is running on port 8080");
