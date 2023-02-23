70d2f66e2a4e05a5b28964f27a0a7e8e9ff42fce
Andrii_Petrov <Andrii_Petrov@epam.com>
2023-02-23 23:56:29 +0100

    Simple websocket server for chat implemented

--- app.js
+++ app.js
@@ -4,46 +4,66 @@ const WebSocketServer = require('ws');
 // Creating a new websocket server
 const wss = new WebSocketServer.Server({ port: 8080 })
 
-var id = 0;
-var lookup = {};
+var clientId = 0;
+var lookup = []
+var uniqueMessageId = 0;
 
-// Creating connection using websocket
+const ACTION_OUT_PLAYERS_UPDATED = "ACTION_OUT_PLAYERS_UPDATED";
+const ACTION_IN_SET_NAME = "ACTION_IN_SET_NAME";
+const ACTION_IN_NEW_CHAT_MESSAGE = "ACTION_IN_NEW_CHAT_MESSAGE";
+const ACTION_OUT_NEW_CHAT_MESSAGE = "ACTION_OUT_NEW_CHAT_MESSAGE";
+const ACTION_OUT_SET_PLAYER_ID = "ACTION_OUT_SET_PLAYER_ID";
+
 wss.on("connection", ws => {
+// Creating connection using websocket
 
-    ws.id = id++;
-    lookup[ws.id] = ws;
+    ws.id = clientId++;
+    lookup.push(ws);
+
+    console.log("the client has connected: " + ws.id + ", connections: " + lookup.map(ws => ws.id));
 
     function send(message) {
-        ws.send(message);
+        ws.send(JSON.stringify(message));
     }
 
     function sendAll(message) {
-        wss.clients.forEach(client => client.send(message.toString()));
+        lookup.forEach(ws => ws.send(JSON.stringify(message)))
+        // wss.clients.forEach(client => client.send(message.toString()));
     }
 
     function sendToClient(message, clientId) {
-        clientWs = lookup[clientId];
-        clientWs.send(message.toString());
+        clientWs = lookup.filter(ws => clientId === ws.id);
+        clientWs.send(JSON.stringify(message));
     }
 
-    console.log("new client connected");
+    function sendUpdatedPlayers() {
+        const players = lookup.map(ws => ({playerId: ws.id, playerName: ws.playerName}));
+        sendAll({action: ACTION_OUT_PLAYERS_UPDATED, data: players});
+    }
 
-    // sending message to client
-    send('Welcome, you are connected!!!!!!');
+    function processIncomingData(clientId, data) {
+        console.log("Client has sent us: " + JSON.stringify(data));
+
+        switch(data.action) {
+            case ACTION_IN_SET_NAME:
+                const playerName = data.data;
+                ws.playerName = playerName;
+                send({action: ACTION_OUT_SET_PLAYER_ID, data: ws.id });
+                sendUpdatedPlayers();
+                break;
+            case ACTION_IN_NEW_CHAT_MESSAGE:
+                const newChatMessageText = data.data;
+                const newChatMessage = {id: uniqueMessageId++, playerId: ws.id, playerName: ws.playerName, chatMessageText: newChatMessageText};
+                sendAll({action: ACTION_OUT_NEW_CHAT_MESSAGE, data: newChatMessage})
+                break;
+            default:
+                send({action: "UNKNOWN_COMMAND", data: data.action});
+        }
+    }
 
     //on message from client
     ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`);
-        // let myVar = null;
-        if(data == 'start') {
-            myVar = setInterval(function(){ timer() }, 1000);
-        } else if (data == 'stop') {
-            clearInterval(myVar);
-        } else {
-            // send(data);
-            // sendToClient(data, 0);
-            sendAll(`Unknown command: ${data}`);
-        }
+        processIncomingData(ws.id, JSON.parse(data));
     });
 
     function timer() {
@@ -56,7 +76,9 @@ wss.on("connection", ws => {
 
     // handling what to do when clients disconnects from server
     ws.on("close", () => {
-        console.log("the client has connected");
+        lookup = lookup.filter(item => item.id !== ws.id);
+        sendUpdatedPlayers();
+        console.log("the client has disconnected: " + ws.id);
     });
 
     // handling client connection error
